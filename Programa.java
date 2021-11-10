6e527cedcb40e51f30cf349935c3425322a17deb
HarturFranco <franco.s.arthur00@gmail.com>
2021-11-10 00:00:55 -0300

    refactor: Mudando para interface grafica.

--- Programa.java
+++ Programa.java
@@ -2,9 +2,10 @@
 public class Programa {
 
 	public static void main(String[] args) {
-		Jogo jogo = new Jogo();
-		
-		jogo.jogar();
+//		Jogo jogo = new Jogo();
+		InterfaceGrafica ig = new InterfaceGrafica();
+		ig.exibir();
+//		jogo.jogar();
 	}
 
 }
