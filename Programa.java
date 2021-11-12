9291a457ac22c3b8d32b6ad511c8a11d26199aa7
HarturFranco <franco.s.arthur00@gmail.com>
2021-11-11 22:46:02 -0300

    fix: Programa

--- Programa.java
+++ Programa.java
@@ -3,9 +3,7 @@ public class Programa {
 
     public static void main(String[] args) {
 		Jogo jogo = new Jogo();
-//        InterfaceGrafica ig = new InterfaceGrafica();
-//        ig.exibir();
-//		jogo.jogar();
+
     }
 
 }
